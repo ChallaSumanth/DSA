9e40105d9e05ba6ee844c9666c127515650e62ca
Sumanth <62050083+ChallaSumanth@users.noreply.github.com>
2023-03-07 22:23:10 +0530

    Update 1345. Jump Game IV.java

--- Leetcode/1345. Jump Game IV.java
+++ Leetcode/1345. Jump Game IV.java
@@ -1,3 +1,31 @@
+class Solution {
+    public int minJumps(int[] arr) {
+        Map<Integer,List<Integer>> map = new HashMap<>();
+        int len = arr.length;
+        for(int i = 0; i < len; i++)
+        {
+            List<Integer> indexes = map.getOrDefault(arr[i],new ArrayList<>());
+            indexes.add(i);
+            map.put(arr[i],indexes);
+        }
+        boolean [] visited = new boolean[len];
+        Queue<Integer> qu = new LinkedList<>();
+        qu.offer(0);
+        int level = 0;
+        while(!qu.isEmpty())
+        {
+            int size = qu.size();
+            while(size-- > 0)
+            {
+                Integer head = qu.poll();
+                if(head == len - 1)
+                    return level;
+                if(head < 0 || head > arr.length - 1 || visited[head])
+                    continue;
+                if(head - 1 > 0 && !visited[head - 1])
+                    qu.offer(head - 1);
+                if(head + 1 > 0 && !visited[head + 1])
+                    qu.offer(head + 1);
 
                 if(map.containsKey(arr[head]))
                 {
@@ -13,6 +41,5 @@
             level++;
         }
         return -1;
-        
     }
 }
