96bce3141c322d79430b988c8ca118434f8cafe7
Challa Sumanth <challasumanth44@gmail.com>
2023-04-01 20:29:25 +0530

    704. Binary Search.java

---
+++ Leetcode/704. Binary Search.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int len = nums.length;
+        int lo = 0;
+        int hi = len - 1;
+        while(lo <= hi)
+        {
+            int mid = lo + (hi - lo) / 2;
+            if(nums[mid] == target)
+                return mid;
+            if(nums[mid] < target)
+                lo = mid + 1;
+            else
+                hi = mid - 1;
+        }
+        return -1;
+    }
+}
