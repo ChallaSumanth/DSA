5537568fc22e463621378349646b3e258eb98dbb
Challa Sumanth <challasumanth44@gmail.com>
2023-04-18 08:53:29 +0530

    1768. Merge Strings Alternately.java

---
+++ Leetcode/1768. Merge Strings Alternately.java
@@ -0,0 +1,21 @@
+class Solution {
+    public String mergeAlternately(String word1, String word2) {
+        int len1 = word1.length();
+        int len2 = word2.length();
+        int i = 0;
+        int j = 0;
+        StringBuilder sb = new StringBuilder();
+        while(i < len1 && j < len2)
+        {
+            if(i < len1)
+                sb.append(word1.charAt(i++));
+            if(j < len2)
+                sb.append(word2.charAt(j++));
+        }
+        if(i != len1)
+            sb.append(word1.substring(i));
+        if(j != len2)
+            sb.append(word2.substring(j));
+        return sb.toString();
+    }
+}
