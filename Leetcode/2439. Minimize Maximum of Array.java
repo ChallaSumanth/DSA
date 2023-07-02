39a04d8726b3c94dfda30817bb5412aa3cc26255
Challa Sumanth <challasumanth44@gmail.com>
2023-07-02 18:42:49 +0530

    leetcode 2439 better

--- Leetcode/2439. Minimize Maximum of Array.java
+++ Leetcode/2439. Minimize Maximum of Array.java
@@ -1,3 +1,33 @@
+//1.
+class Solution {
+    public int minimizeArrayValue(int[] nums) {
+        int lo = 0;
+        int hi = 0;
+        for(int num : nums)
+            hi = Math.max(hi, num);
+        while(lo < hi)
+        {
+            int mid = (lo + hi) >> 1;
+            if(isValid(mid, nums))
+                hi = mid;
+            else
+                lo = mid + 1;
+        }
+        return lo;
+    }
+    private boolean isValid(int x, int[] nums)
+    {
+        long sum = 0;
+        for(int i = 0; i < nums.length; i++)
+        {
+            sum += nums[i];
+            if(sum > (long) x * (i + 1)) return false;
+        }
+        return true;
+    }
+}
+
+//2.optimal
 class Solution {
     public int minimizeArrayValue(int[] nums) {
         long prefixSum = 0;
