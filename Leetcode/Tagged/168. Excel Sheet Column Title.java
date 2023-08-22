1f15dc24754ff1716598dc28bcf84f22e46afc22
Challa Sumanth <challasumanth44@gmail.com>
2023-08-22 09:57:31 +0530

    168. Excel Sheet Column Title.java

---
+++ Leetcode/Tagged/168. Excel Sheet Column Title.java
@@ -0,0 +1,13 @@
+//Microsoft 4 Google 2 Amazon 2
+class Solution {
+    public String convertToTitle(int columnNumber) {
+        StringBuilder sb = new StringBuilder();
+        while(columnNumber > 0)
+        {
+            columnNumber--;
+            sb.insert(0, (char)('A' + columnNumber % 26));
+            columnNumber /= 26;
+        }
+        return sb.toString();
+    }
+}
