74a9aad60dc872bba7306ebdbde96fe6f3837051
Sumanth <challasumanth44@gmail.com>
2023-12-15 19:34:00 +0530

    1436. Destination City.java

---
+++ Leetcode/1436. Destination City.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String destCity(List<List<String>> paths) {
+        Set<String> start = new HashSet<>();
+        for(List<String> path : paths)
+            start.add(path.get(0));
+        for(List<String> path : paths)
+        {
+            if(!start.contains(path.get(1)))
+                return path.get(1);
+        }
+        return null;
+    }
+}
